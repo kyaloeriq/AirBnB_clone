e29184539c9c5a1ccdbb8ad896335f0c65e5d06c
kyaloeriq <kyaloeriq@gmail.com>
2024-03-08 08:30:16 -0800

    the Airbnb Console class

--- models/console.py
+++ models/console.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
+import cmd
+
+class MyCmd(cmd.Cmd):
+    def do_hello(self, arg):
+        print("Hello,", arg)
+
+    def do_quit(self, arg):
+        return True
+
 if __name__ == '__main__':
     ABNBCmd().cmdloop()
