566ca3e4e655301e8420af3e5e5df3bf946c1451
kyaloeriq <kyaloeriq@gmail.com>
2024-03-09 03:22:36 -0800

    the console class

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import cmd
-from hbnb_cmd import HBNBCommand  # Import the HBNBCommand class from another file
+from hbnb_cmd import HBNBCommand  # Import the HBNBCommand class
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
