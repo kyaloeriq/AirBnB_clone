1a2a3936cdcd744643102bdf15c72a2422680599
kyaloeriq <kyaloeriq@gmail.com>
2024-03-09 05:40:32 -0800

    the FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,30 +10,42 @@ class FileStorage:
     __objects = {}
 
 
-    def all(self):
+    @classmethod
+    def all(cls):
         """Returns the dictionary __objects"""
-        return FileStorage.__objects
+        return cls.__objects
 
-    def new(self, obj):
+    @classmethod
+    def new(cls, obj):
         """Sets in __objects obj with key <obj class name>.id."""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        key = f"{type(obj).__name__}.{obj.id}"
+        cls.__objects[key] = obj
 
-    def save(self):
+    @classmethod
+    def save(cls):
         """Serializes __objects to the JSON file __file_path."""
         serialized_objects = {}
-        for key, obj in FileStorage.__objects.items():
+        for key, obj in cls.__objects.items():
             serialized_objects[key] = obj.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
+        with open(cls.__file_path, 'w') as file:
             json.dump(serialized_objects, file)
 
-    def reload(self):
+     @classmethod
+     def reload(cls):
         """De-serializes JSON file __file_path to __objects, if it exists"""
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
+        if os.path.exists(cls.__file_path):
+            with open(cls.__file_path, 'r') as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    print("Error: JSON file is corrupted.")
+                    return
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    cls = eval(class_name)
-                    obj = cls(**value)
-                    FileStorage.__objects[key] = obj
+                    try:
+                        cls_ = getattr(__import__('models.' + class_name.lower()), class_name)
+                    except AttributeError:
+                        print(f"Error: Class {class_name} not found.")
+                        continue
+                    obj = cls_(**value)
+                    cls.__objects[key] = obj
