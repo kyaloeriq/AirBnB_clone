6c95dda52604dfdfc0a05874655987fc1f23ff48
kyaloeriq <kyaloeriq@gmail.com>
2024-03-09 04:38:12 -0800

    the FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,26 +11,23 @@ from models.review import Review
 from models.place import Place
 
 class FileStorage:
-    """A representation of an abstracted storage engine
+    """A class for serializing instances to a JSON file and deserializing JSON file to instances."""
 
-    Attributes:
-    __file_path (str): Name of the file to save Objects to
-    __objects (dict): Instantiated objects"""
     __file_path = "file.json"
     __objects = {}
-    classes = ["BaseModel", "User", "State",
-               "City", "Amenity", "Place", "Review"]
+
 
     def all(self):
         """Returns the dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj class name>.id."""
+        """Sets in __objects obj with key <obj class name>.id."""
         key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
+        """Serializes __objects to the JSON file __file_path."""
         serialized_objects = {}
         for key, obj in FileStorage.__objects.items():
             serialized_objects[key] = obj.to_dict()
@@ -38,15 +35,12 @@ class FileStorage:
             json.dump(serialized_objects, file)
 
     def reload(self):
-        """De-serialize JSON file __file_path to __objects, if it exists."""
+        """De-serializes JSON file __file_path to __objects, if it exists"""
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as file:
                 data = json.load(file)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    if class_name == "User":
-                        cls = User
-                    else:
-                        cls = eval(class_name)
+                    cls = eval(class_name)
                     obj = cls(**value)
                     FileStorage.__objects[key] = obj
