c71a5affb6674dcf56c3e3018267f8b30a987eeb
kyaloeriq <kyaloeriq@gmail.com>
2024-03-09 05:50:51 -0800

    the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """The BaseModel"""
-import models
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -21,7 +19,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            storage.new(self)  # Call new(self) method on storage
 
     def __str__(self):
         """Return a string representation of the instance"""
@@ -39,4 +36,3 @@ class BaseModel:
     def save(self):
         """Update updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()  # Call save() method of storage
