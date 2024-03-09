e17e72c2e63bbc48e4b0c89064695c29f04029eb
kyaloeriq <kyaloeriq@gmail.com>
2024-03-09 05:21:47 -0800

    the FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,6 @@
 """The FileStorage class"""
 import os
 import json
-from models.base_model import BaseModel
-from models.state import State
-from models.user import User
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from models.place import Place
 
 class FileStorage:
     """A class for serializing instances to a JSON file and deserializing JSON file to instances."""
