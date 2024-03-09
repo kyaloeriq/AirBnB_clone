b41bbd467723e2b0f26230db0fbddbe660556840
kyaloeriq <kyaloeriq@gmail.com>
2024-03-08 23:35:34 -0800

    the command interpreter

--- console.py
+++ console.py
@@ -2,12 +2,5 @@
 import cmd
 from hbnb_cmd import HBNBCommand  # Import the HBNBCommand class from another file
 
-class MyCmd(cmd.Cmd):
-    def do_hello(self, arg):
-        print("Hello,", arg)
-
-    def do_quit(self, arg):
-        return True
-
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
