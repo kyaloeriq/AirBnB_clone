254c165fa691bbaa334c1e52fcc511e963424cb2
kyaloeriq <kyaloeriq@gmail.com>
2024-03-09 03:37:58 -0800

    the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -14,15 +14,20 @@ class BaseModel:
                 if key == 'updated_at' or key == 'created_at':
                     setattr(self, key,
                             datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
+                else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+
+    def __str__(self):
+        """Return a string representation of the instance"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def to_dict(self):
         """Return a dictionary representation of the instance"""
+        obj_dict = self.__dict__.copy()
         obj_dict = self.__dict__.copy()
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
@@ -31,10 +36,3 @@ class BaseModel:
     def save(self):
         """Update the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
-
-    def __str__(self):
-        """Return a string representation of the instance"""
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.to_dict())
-
