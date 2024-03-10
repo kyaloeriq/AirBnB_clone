a878a55bcbd3a3ec7c4d0f6370ef644328532256
kyaloeriq <kyaloeriq@gmail.com>
2024-03-10 10:13:40 -0700

    entry point of the command interpreter

--- console.py
+++ console.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python3
 import cmd
-from hbnb_cmd import HBNBCommand  # Import the HBNBCommand class
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "  # Setting custom prompt
+
+    def do_quit(self, arg):
+        """Command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Handles EOF signal to exit the program"""
+        print()  # Add a newline before exiting
+        return True
+
+    def emptyline(self):
+        """Called when an empty line is entered"""
+        pass  # Do nothing, just return to prompt
+
+    def help_quit(self):
+        """Documentation for the quit command"""
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        """Help documentation for handling EOF"""
+        print("Handles EOF signal to exit the program")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
