8f5d2d7648291226a50fead3e7a1b1d0644f62c5
kyaloeriq <kyaloeriq@gmail.com>
2024-03-12 19:31:15 -0700

    the User class

--- models/user.py
+++ models/user.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-"""Class User that inherits from BaseModel"""
+"""Module containing the User class"""
+
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    """User class"""
+    """
+    User class.
+    Attributes:
+        email (str): User's email address.
+        password (str): User's password.
+        first_name (str): User's first name.
+        last_name (str): User's last name.
+    """
     email = ""
     password = ""
     first_name = ""
